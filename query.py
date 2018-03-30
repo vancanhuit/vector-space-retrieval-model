7b0dbdef7606417fe766c19e2182316173d0ecd1
vancanhuit <vancanhuit@gmail.com>
2018-03-30 19:11:49 +0700

    Perform query

---
+++ query.py
@@ -0,0 +1,47 @@
+import pickle
+import os
+import sys
+import math
+from utils import textprocessing
+from utils import helpers
+from collections import Counter
+''' Query '''
+
+docs_file = os.path.join(os.getcwd(), 'data', 'docs.pickle')
+inverted_index_file = os.path.join(
+    os.getcwd(), 'data', 'inverted_index.pickle')
+
+stopwords_file = os.path.join(os.getcwd(), 'resources', 'stopwords_en.txt')
+
+# Deserialize data
+with open(docs_file, 'rb') as f:
+    docs = pickle.load(f)
+with open(inverted_index_file, 'rb') as f:
+    inverted_index = pickle.load(f)
+
+with open(stopwords_file, 'r') as f:
+    stopwords = set(f.read().split())
+
+dictionary = set(inverted_index.keys())
+
+# Get query from command line
+query = sys.argv[1]
+# Preprocess query
+query = textprocessing.preprocess_text(query, stopwords)
+query = [word for word in query if word in dictionary]
+query = Counter(query)
+
+# Compute weights for words in query
+for word, value in query.items():
+    query[word] = inverted_index[word]['idf'] * (1 + math.log(value))
+
+scores = [[i, 0] for i in range(len(docs))]
+for word, value in query.items():
+    for doc in inverted_index[word]['postings_list']:
+        index, weight = doc
+        scores[index][1] += value * weight
+
+scores.sort(key=lambda doc: doc[1], reverse=True)
+print(scores)
+for index, score in enumerate(scores):
+    print('{}. {} - {}'.format(index + 1, docs[score[0]], score[1]))
